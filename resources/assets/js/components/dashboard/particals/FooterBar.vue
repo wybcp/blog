793e3ce110f4db7c2c5d29669731dfb1f8c72c27
Riverside <wangyb65@gmail.com>
2018-01-20 13:38:45 +0800

    fix remote bug

--- resources/assets/js/components/dashboard/particals/FooterBar.vue
+++ resources/assets/js/components/dashboard/particals/FooterBar.vue
@@ -2,10 +2,6 @@
     <footer>
         <div class="copy-right text-center">
             <span>© Pig Jian 2016. All rights reserved. </span>
-<<<<<<< HEAD
-            <!--<a class="item" href="http://www.miitbeian.gov.cn/">粤ICP备16020344号-1</a>-->
-=======
->>>>>>> 4f383f004e6158d93f8bd73fa7d2bed2d2138929
         </div>
     </footer>
 </template>
