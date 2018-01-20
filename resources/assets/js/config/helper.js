a13c433582ab06e111499a54d4807671e27ab7df
Riverside <wangyb65@gmail.com>
2018-01-20 10:47:27 +0800

    fix pull bug

--- resources/assets/js/config/helper.js
+++ resources/assets/js/config/helper.js
@@ -2,11 +2,8 @@ export function stack_error(response) {
     if (typeof response.data === 'string') {
         toastr.error(response.status + ' ' + response.statusText)
     } else {
-<<<<<<< HEAD
-        let data = response.data;
-=======
+
         let data = response.data.errors
->>>>>>> 9a115298f573500dcc8f72745b3b4a195c45bf4f
         let content = '';
 
         Object.keys(data).map(function(key, index) {
